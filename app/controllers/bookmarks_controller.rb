d1e9d8587962c802e8989e96c4ce054f2dad4bfd
Yaya Rezai <yayarezai3@gmail.com>
2023-05-26 16:46:04 +0200

    all tests are passing just need to fix some styling issue

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,27 +1,46 @@
 class BookmarksController < ApplicationController
 
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
+  # def create
+  #   @bookmark = Bookmark.new(bookmark_params)
+  #   if @bookmark.save
+  #     redirect_to @bookmark.list, notice: 'Bookmark was successfully created.'
+  #   else
+  #     render :new
+  #   end
+  # end
+
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     if @bookmark.save
-      redirect_to @bookmark.list, notice: 'Bookmark was successfully created.'
+      redirect_to list_path(@list)
     else
       render :new
     end
   end
 
+  # def destroy
+  #   @bookmark = Bookmark.find(params[:id])
+  #   @bookmark.destroy
+  #   redirect_to @bookmark.list, notice: 'Bookmark was successfully deleted'
+  # end
+
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to @bookmark.list, notice: 'Bookmark was successfully deleted'
+
+    redirect_to list_path(@bookmark.list)
   end
 
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:title, :url, :list_id)
+    params.require(:bookmark).permit(:comment, :url, :movie_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # get "/lists", to: "lists#index"
-  resources :bookmarks, only: [:new, :create, :destroy]
-  resources :lists, only: [:index, :new, :show, :create]
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+#   # get "/lists", to: "lists#index"
+#   resources :bookmarks, only: [:new, :create, :destroy]
+#   resources :lists, only: [:index, :new, :show, :create]
+# end
+Rails.application.routes.draw do
+  root to: 'lists#index'
+  resources :lists, only: [:index, :new, :show, :create, :destroy] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
+  resources :movies, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
